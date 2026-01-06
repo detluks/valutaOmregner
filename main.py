59d4b7247d6c9f1ed52c41d3787b0ed35b4f92db
Beck <jonasbeck06@gmail.com>
2026-01-06 15:16:34 +0100

    små fejl

--- main.py
+++ main.py
@@ -95,6 +95,6 @@ if __name__ == "__main__":
     view = View(root)
     controller = Controller(model, view)
     view.set_controller(controller)
-    model.get_valutas()
+    model.getValutas()
 
     root.mainloop()
